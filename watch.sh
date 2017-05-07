557ee23f6565e72de00517e897350f40cdf16e92
Hans Ole Hatzel <hhatzel@gmail.com>
2017-05-07 11:48:59 +0200

    Watch starts binary in server mode

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watchexec --exts rs,toml,sql --restart "pwd && ./reset.sh && RUST_BACKTRACE=1 cargo run"
+watchexec --exts rs,toml,sql --restart "pwd && ./reset.sh && RUST_BACKTRACE=1 cargo run -- serve"

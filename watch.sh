bee18458a30f0a0e449754538818df830d398134
Hans Ole Hatzel <hhatzel@gmail.com>
2017-05-18 17:58:59 +0200

    Let watch run the tests for now

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watchexec --exts rs,toml,sql --restart "pwd && ./reset.sh && RUST_BACKTRACE=1 cargo run -- serve"
+watchexec --exts rs,toml,sql --restart "pwd && ./reset.sh && RUST_BACKTRACE=1 cargo test -- --test-threads 4"

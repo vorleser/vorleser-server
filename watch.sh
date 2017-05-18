0f065073cbadbdefbdaebdf29063c58855edce70
Hans Ole Hatzel <hhatzel@gmail.com>
2017-05-18 19:54:52 +0200

    One thread for testing!

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watchexec --exts rs,toml,sql --restart "pwd && ./reset.sh && RUST_BACKTRACE=1 cargo test -- --test-threads 4"
+watchexec --exts rs,toml,sql --restart "pwd && ./reset.sh && RUST_BACKTRACE=1 cargo test -- --test-threads 1"

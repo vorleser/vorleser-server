a71b55245fedf2683312834cbca90214b927feec
Hans Ole Hatzel <hhatzel@gmail.com>
2017-06-21 19:51:24 +0200

    Tests compile again

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use helpers::db::init_db_pool;
 use helpers;
 use diesel::prelude::*;
 use models::user::UserModel;
-use rocket::testing::MockRequest;
+use rocket::local::Client;
 use rocket::http::{Status, Method, Header, ContentType};
 use serde_json::{self, Value};
 
@@ -12,22 +12,26 @@ describe! api_tests {
         let conn = &*pool.get().unwrap();
         conn.execute("TRUNCATE audiobooks, chapters, playstates, users RESTART IDENTITY CASCADE").unwrap();
         let rocket = helpers::rocket::factory(pool);
+        let client = Client::new(rocket).unwrap();
         let user = UserModel::create(&"test@test.test", &"lol", conn).expect("Error saving user");
     }
 
     it "should let you login" {
         let data = json!({"email": "test@test.test", "password": "lol"});
-        let mut req = MockRequest::new(Method::Post, "/api/auth/login")
+        println!("{}", data.to_string());
+        let mut res = client.post("/api/auth/login")
                 .header(ContentType::JSON)
-                .body(data.to_string());
-        let mut res = req.dispatch_with(&rocket);
+                .body(data.to_string())
+                .dispatch();
         assert_eq!(res.status(), Status::Ok);
 
         let data: Value = serde_json::from_str(&res.body().unwrap().into_string().unwrap()).unwrap();
         let secret = &data.get("id").unwrap().as_str().unwrap();
-        let mut req2 = MockRequest::new(Method::Get, "/api/auth/whoami")
-                .header(Header::new("Authorization", secret.to_string()));
-        let mut whoami_res = req2.dispatch_with(&rocket);
-        assert_eq!(whoami_res.status(), Status::Ok);
+        let mut res2 = client.post("/api/auth/login")
+                .header(ContentType::JSON)
+                .header(Header::new("Authorization", secret.to_string()))
+                .body(data.to_string())
+                .dispatch();
+        assert_eq!(res2.status(), Status::Ok);
     }
 }

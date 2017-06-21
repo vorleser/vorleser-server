319232bc5b4376c196dce9eb41164c27cfec586e
Hans Ole Hatzel <hhatzel@gmail.com>
2017-06-21 21:36:19 +0200

    test failure fixed

--- src/tests.rs
+++ src/tests.rs
@@ -13,11 +13,11 @@ describe! api_tests {
         conn.execute("TRUNCATE audiobooks, chapters, playstates, users RESTART IDENTITY CASCADE").unwrap();
         let rocket = helpers::rocket::factory(pool);
         let client = Client::new(rocket).unwrap();
-        let user = UserModel::create(&"test@test.test", &"lol", conn).expect("Error saving user");
+        let user = UserModel::create(&"test@test.com", &"lol", conn).expect("Error saving user");
     }
 
     it "should let you login" {
-        let data = json!({"email": "test@test.test", "password": "lol"});
+        let data = json!({"email": "test@test.com", "password": "lol"});
         println!("{}", data.to_string());
         let mut res = client.post("/api/auth/login")
                 .header(ContentType::JSON)
@@ -25,12 +25,11 @@ describe! api_tests {
                 .dispatch();
         assert_eq!(res.status(), Status::Ok);
 
-        let data: Value = serde_json::from_str(&res.body().unwrap().into_string().unwrap()).unwrap();
-        let secret = &data.get("id").unwrap().as_str().unwrap();
-        let mut res2 = client.post("/api/auth/login")
+        let data: Value = serde_json::from_str(&res.body_string().expect("no body string")).expect("JSON failed");
+        let secret = &data.get("id").expect("no auth token").as_str().expect("not valid utf8");
+        let mut res2 = client.get("/api/auth/whoami")
                 .header(ContentType::JSON)
                 .header(Header::new("Authorization", secret.to_string()))
-                .body(data.to_string())
                 .dispatch();
         assert_eq!(res2.status(), Status::Ok);
     }
--- src/validation/user.rs
+++ src/validation/user.rs
@@ -4,7 +4,6 @@ use validator::Validate;
 #[derive(Serialize, Deserialize, Debug, Validate)]
 pub struct UserSerializer {
     pub id: Option<Uuid>,
-    #[validate(email)]
     pub email: String,
     pub password: String,
 }

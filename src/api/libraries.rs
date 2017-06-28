8894497fb7253f70159cc0e2f50e25cab3a08dde
Hans Ole Hatzel <hhatzel@gmail.com>
2017-06-28 21:28:17 +0200

    Changes to api for moved playstates

--- src/api/libraries.rs
+++ src/api/libraries.rs
@@ -7,7 +7,7 @@ use helpers::db::DB;
 use models::library::Library;
 use models::audiobook::Audiobook;
 use models::chapter::Chapter;
-use models::audiobook::Playstate;
+use models::playstate::Playstate;
 
 #[get("/libraries")]
 pub fn libraries(current_user: UserModel, db: DB) -> APIResponse {
@@ -21,6 +21,7 @@ pub fn all_the_things(current_user: UserModel, db: DB) -> APIResponse {
     use schema;
     let libs = current_user.accessible_libraries(&*db).unwrap();
     let books = current_user.accessible_audiobooks(&*db).unwrap();
+    let playstates = current_user.accessible_playstates(&*db).unwrap();
     let chapters: Vec<Chapter> = books.clone().into_iter().flat_map(|b| Chapter::belonging_to(&b).load::<Chapter>(&*db).unwrap()).collect();
     ok().data(json!({
         "libraries": libs,

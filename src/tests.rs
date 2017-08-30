810c791ca903c9940f23311c5cc10d1f4365feca
Hans Ole Hatzel <hhatzel@gmail.com>
2017-08-30 20:24:32 +0200

    Crashing the nightly rustc 1.21.0-nightly (c11f689d2 2017-08-29)

--- src/tests.rs
+++ src/tests.rs
@@ -2,9 +2,33 @@ use helpers::db::init_db_pool;
 use helpers;
 use diesel::prelude::*;
 use models::user::UserModel;
-use rocket::local::Client;
+use rocket::local::{Client, LocalResponse};
+use rocket::Response;
 use rocket::http::{Status, Method, Header, ContentType};
 use serde_json::{self, Value};
+
+fn post<'a>(client: &'a Client, url: &'a str, data: &Value) -> LocalResponse<'a> {
+    client.post(url)
+        .header(ContentType::JSON)
+        .body(data.to_string())
+        .dispatch()
+}
+
+fn get<'a>(client: &'a Client, url: &'a str) -> LocalResponse<'a> {
+    client.get(url).dispatch()
+}
+
+#[test]
+fn lol() {
+    let mut pool = init_db_pool();
+    let conn = &*pool.get().unwrap();
+    conn.execute("TRUNCATE audiobooks, chapters, playstates, users RESTART IDENTITY CASCADE").unwrap();
+    let rocket = helpers::rocket::factory(pool);
+    let client = Client::new(rocket).unwrap();
+    let user = UserModel::create(&"test@test.com", &"lol", conn).expect("Error saving user");
+
+    let result = get(&client, "/api/auth/whoami");
+}
 
 describe! api_tests {
     before_each {
@@ -19,18 +43,12 @@ describe! api_tests {
     it "should let you login" {
         let data = json!({"email": "test@test.com", "password": "lol"});
         println!("{}", data.to_string());
-        let mut res = client.post("/api/auth/login")
-                .header(ContentType::JSON)
-                .body(data.to_string())
-                .dispatch();
+        let mut res = post(&client, "/api/auth/login", &data);
         assert_eq!(res.status(), Status::Ok);
 
         let data: Value = serde_json::from_str(&res.body_string().expect("no body string")).expect("JSON failed");
         let secret = &data.get("id").expect("no auth token").as_str().expect("not valid utf8");
-        let mut res2 = client.get("/api/auth/whoami")
-                .header(ContentType::JSON)
-                .header(Header::new("Authorization", secret.to_string()))
-                .dispatch();
-        assert_eq!(res2.status(), Status::Ok);
+        // uncommenting this line will crash the compiler
+        let lololol = get(&client, "/api/auth/whoami")
     }
 }

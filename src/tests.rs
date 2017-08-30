04d559ddcd89411c0422868c3ef7c7180992b861
Hans Ole Hatzel <hhatzel@gmail.com>
2017-08-30 20:48:11 +0200

    Add authirzation to get and post helper functions

--- src/tests.rs
+++ src/tests.rs
@@ -7,15 +7,30 @@ use rocket::Response;
 use rocket::http::{Status, Method, Header, ContentType};
 use serde_json::{self, Value};
 
-fn post<'a>(client: &'a Client, url: &'a str, data: &Value) -> LocalResponse<'a> {
-    client.post(url)
-        .header(ContentType::JSON)
-        .body(data.to_string())
-        .dispatch()
+fn post<'a>(client: &'a Client, url: &'a str, data: &Value, auth: Option<&str>) -> LocalResponse<'a> {
+    if let Some(token) = auth {
+        client.post(url)
+            .header(Header::new("Authorization", token.to_owned()))
+            .header(ContentType::JSON)
+            .body(data.to_string())
+            .dispatch()
+    } else {
+        client.post(url)
+            .header(ContentType::JSON)
+            .body(data.to_string())
+            .dispatch()
+    }
 }
 
-fn get<'a>(client: &'a Client, url: &'a str) -> LocalResponse<'a> {
-    client.get(url).dispatch()
+fn get<'a>(client: &'a Client, url: &'a str, auth: Option<&str>) -> LocalResponse<'a> {
+    if let Some(token) = auth {
+        client.get(url)
+            .header(Header::new("Authorization", token.to_owned()))
+            .dispatch()
+    } else {
+        client.get(url)
+            .dispatch()
+    }
 }
 
 describe! api_tests {
@@ -31,12 +46,27 @@ describe! api_tests {
     it "should let you login" {
         let data = json!({"email": "test@test.com", "password": "lol"});
         println!("{}", data.to_string());
-        let mut res = post(&client, "/api/auth/login", &data);
+        let mut res = post(&client, "/api/auth/login", &data, None);
         assert_eq!(res.status(), Status::Ok);
 
         let data: Value = serde_json::from_str(&res.body_string().expect("no body string")).expect("JSON failed");
         let secret = &data.get("id").expect("no auth token").as_str().expect("not valid utf8");
-        let res2 = get(&client, "/api/auth/whoami");
+        let res2 = get(&client, "/api/auth/whoami", Some(secret));
         assert_eq!(res2.status(), Status::Ok);
+    }
+
+    it "should not let you in with the wrong username or password" {
+        let data = json!({"email": "test@test.com", "password": "lola"});
+        let mut res = post(&client, "/api/auth/login", &data, None);
+        assert_eq!(res.status(), Status::Unauthorized);
+        let data = json!({"email": "test@testa.com", "password": "lol"});
+        let mut res2 = post(&client, "/api/auth/login", &data, None);
+        assert_eq!(res2.status(), Status::Unauthorized);
+    }
+
+    it "should not work with a wrong auth token" {
+        let res = get(&client, "/api/auth/whoami", Some("secret"));
+        assert_eq!(res.status(), Status::BadRequest);
+        // TODO: test with valid uuid, result should then be unauthorized
     }
 }

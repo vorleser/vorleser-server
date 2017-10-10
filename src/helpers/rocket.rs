7a9f8d6ff439767967daf0aced6af3fe032e6bc2
Hans Ole Hatzel <hhatzel@gmail.com>
2017-10-10 21:03:33 +0200

    CORS headers and options method

--- src/helpers/rocket.rs
+++ src/helpers/rocket.rs
@@ -2,9 +2,51 @@ use rocket::{self, Rocket};
 use ::api;
 use ::handlers;
 
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Header, ContentType, Method};
+use std::io::Cursor;
+use std::path::PathBuf;
+
+pub struct CORS();
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response
+        }
+    }
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON) {
+            response.set_header(Header::new("Access-Control-Allow-Origin", "http://localhost:9901"));
+            response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PUT"));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
+            // response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        }
+
+        if request.method() == Method::Options {
+            response.set_header(ContentType::Plain);
+            response.set_sized_body(Cursor::new(""));
+        }
+    }
+}
+
+#[route(OPTIONS, "/<path..>")]
+fn options_handler<'a>(path: PathBuf) -> Response<'a> {
+    Response::build()
+        .raw_header("Access-Control-Allow-Origin", "http://localhost:9901")
+        .raw_header("Access-Control-Allow-Methods", "OPTIONS, POST, PUT")
+        .raw_header("Access-Control-Allow-Headers", "Content-Type")
+        .finalize()
+}
+
 pub fn factory(pool: super::db::Pool) -> Rocket {
     rocket::ignite()
+        .attach(CORS())
         .manage(pool)
+        .mount("/", routes![options_handler])
         .mount("/", routes![api::audiobooks::data_file])
         .mount("/api/", routes![
             api::libraries::libraries,

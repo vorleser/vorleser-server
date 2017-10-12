49c54df836231ee8fcedf50a54e2bd2165cbecd2
Hans Ole Hatzel <hhatzel@gmail.com>
2017-10-12 19:59:18 +0200

    Number of test threads doesn't matter anymore

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watchexec --exts rs,toml,sql --restart "pwd && ./reset.sh && cargo build && RUST_BACKTRACE=1 cargo test -- --test-threads 1"
+watchexec --exts rs,toml,sql --restart "pwd && ./reset.sh && cargo build && RUST_BACKTRACE=1 cargo test"

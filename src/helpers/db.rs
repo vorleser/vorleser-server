7b6e3ad2831197e99a763b9c5e9bacbcb5b2a5f2
Hans Ole Hatzel <hhatzel@gmail.com>
2017-12-10 23:05:34 +0100

    Set busy timeout for SQLite

--- src/helpers/db.rs
+++ src/helpers/db.rs
@@ -3,14 +3,27 @@ use rocket::http::Status;
 use rocket::{Request, State, Outcome};
 use rocket::request::{self, FromRequest};
 use diesel::sqlite::SqliteConnection;
-use r2d2;
 use r2d2_diesel::ConnectionManager;
+use r2d2::{self, CustomizeConnection};
 use std::env;
 use dotenv::dotenv;
+use diesel::dsl::sql;
+use diesel::{self, ExecuteDsl};
 
 pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 pub type PooledConnection = r2d2::PooledConnection<ConnectionManager<SqliteConnection>>;
 pub type Connection = SqliteConnection;
+
+/// Our connection_customizer will set timeout behavior on the SQLite connection
+#[derive(Copy, Clone, Debug)]
+pub struct BusyWaitConnectionCustomizer;
+
+impl<C: diesel::Connection, E> CustomizeConnection<C, E> for BusyWaitConnectionCustomizer {
+    fn on_acquire(&self, conn: &mut C) -> Result<(), E> {
+        conn.batch_execute("PRAGMA journal_mode=WAL; PRAGMA busy_timeout = 5000;").unwrap();
+        Ok(())
+    }
+}
 
 /// Initialize database DB pool from specified URL.
 /// Will fall back to "DATABASE_URL" environment variable if `url` is None.
@@ -20,7 +33,10 @@ pub fn init_db_pool(url: Option<String>) -> Pool {
         env::var("DATABASE_URL").expect("DATABASE_URL must be set")
     );
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
-    r2d2::Pool::builder().build(manager).expect("Failed to create pool.")
+    r2d2::Pool::builder()
+        .connection_customizer(Box::new(BusyWaitConnectionCustomizer{}))
+        .build(manager)
+        .expect("Failed to create pool.")
 }
 
 #[cfg(test)]
@@ -28,8 +44,12 @@ pub fn init_test_db_pool() -> Pool {
     use diesel::Connection;
     dotenv().unwrap();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
-    let pool = r2d2::Pool::builder().max_size(1).build(manager).expect("Failed to create pool.");
+    let manager = ConnectionManager::<SqliteConnection>::new("data/vorleser.sqlite");
+    let pool = r2d2::Pool::builder()
+        .connection_customizer(Box::new(BusyWaitConnectionCustomizer{}))
+        .max_size(1)
+        .build(manager)
+        .expect("Failed to create pool.");
     (*pool.get().unwrap()).begin_test_transaction();
     pool
 }
